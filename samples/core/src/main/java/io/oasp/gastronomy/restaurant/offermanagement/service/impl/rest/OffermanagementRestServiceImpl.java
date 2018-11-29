2dd1b0c607002715a911caad829c3040ed2e97ec
wswiders <wojtek.swiderski8@gmail.com>
2018-11-29 11:51:51 +0100

    added implementation of REST CRUD methods for special entity

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/service/impl/rest/OffermanagementRestServiceImpl.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/service/impl/rest/OffermanagementRestServiceImpl.java
@@ -32,6 +32,8 @@ import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.ProductFilter;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.ProductSearchCriteriaTo;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.ProductSortBy;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.SideDishEto;
+import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.SpecialEto;
+import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.SpecialSearchCriteriaTo;
 import io.oasp.gastronomy.restaurant.offermanagement.service.api.rest.OffermanagementRestService;
 import io.oasp.module.jpa.common.api.to.PaginatedListTo;
 
@@ -221,6 +223,36 @@ public class OffermanagementRestServiceImpl implements OffermanagementRestServic
   public PaginatedListTo<ProductEto> findProductEtosByPost(ProductSearchCriteriaTo searchCriteriaTo) {
 
     return this.offermanagement.findProductEtos(searchCriteriaTo);
+  }
+
+  @Override
+  public SpecialEto getSpecial(long id) {
+
+    return this.offermanagement.findSpecial(id);
+  }
+
+  @Override
+  public List<SpecialEto> findAllSpecials() {
+
+    return this.offermanagement.findAllSpecials();
+  }
+
+  @Override
+  public void deleteSpecial(Long specialId) {
+
+    this.offermanagement.deleteOffer(specialId);
+  }
+
+  @Override
+  public SpecialEto saveSpecial(SpecialEto special) {
+
+    return this.offermanagement.saveSpecial(special);
+  }
+
+  @Override
+  public List<SpecialEto> findSpecialsEtos(SpecialSearchCriteriaTo criteria) {
+
+    return this.offermanagement.findSpecialsEtos(criteria);
   }
 
 }
